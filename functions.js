671004a1e2172423ae59023ea6a45b460412059d
NgocTrann <116326992+NgocTrann@users.noreply.github.com>
2022-11-06 22:31:20 -0500

    Delete functions.js

--- functions.js
+++
@@ -1,8 +0,0 @@
-// When the user clicks on the button, scroll to the top of the document
-function homeFunction() {
-    document.getElementById("home").scrollIntoView();
-}
-
-function experienceFunction() {
-    document.getElementById("experience").scrollIntoView();
-}
