2cdf4f831969381f85299fc6b0a0cfac964d41c8
NgocTrann <116326992+NgocTrann@users.noreply.github.com>
2022-11-07 22:39:56 -0500

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,8 @@
+// When the user clicks on the button, scroll to the top of the document
+function homeFunction() {
+    document.getElementById("home").scrollIntoView();
+}
+
+function experienceFunction() {
+    document.getElementById("experience").scrollIntoView();
+}
